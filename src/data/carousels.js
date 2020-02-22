86f1cade881f7ba8b46a639817e7f62a67551953
Krizza Bullecer <krizzabullecer@gmail.com>
2020-02-23 02:20:52 +0800

    add carousel data

---
+++ src/data/carousels.js
@@ -0,0 +1,16 @@
+export const carousels = {
+    carousels: [
+        {
+            "id": 0,
+            "title": "Live and dine with our friends",
+        },
+        {
+            "id": 1,
+            "title": "See the world through their lens",
+        },
+        {
+            "id": 2,
+            "title": "Be the help that they need",
+        },
+    ]
+}
