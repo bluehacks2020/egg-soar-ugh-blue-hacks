356e11942dfba8ebed39a23c72f5b22f2044525f
Krizza Bullecer <krizzabullecer@gmail.com>
2020-02-22 20:07:19 +0800

    initialize render article function

--- src/component/pages/HomePage/HomePage.js
+++ src/component/pages/HomePage/HomePage.js
@@ -7,16 +7,25 @@ import './style.css'
 
 const HomePage = () => {
   const data = locations.locations;
+  console.log(data)
 
   const renderLocationCards = () => (
-    data.map(loc => <LocationCard key={loc.id} id={loc.id} location={loc.location} />)
+    data.map(loc => <LocationCard key={loc.id} id={loc.id} location={loc.location} community={loc.community} />)
+  )
+
+  const renderArticles = () => (
+    <div></div>
   )
 
   return (
     <div className="HomePage">
       <MyCarousel />
-      <h2>Locations</h2>
+      <h2 className="mt-4">Locations</h2>
+      <hr/>
       { renderLocationCards() }
+      <h2 className="mt-4">Articles</h2>
+      <hr/>
+      { renderArticles() }
     </div>
   )
 }
