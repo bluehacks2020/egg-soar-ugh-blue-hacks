f08f102f911e735735b5550a97de6b0c15c207f6
Krizza Bullecer <krizzabullecer@gmail.com>
2020-02-23 02:21:04 +0800

    dynamic carousel data

--- src/component/MyCarousel/MyCarousel.js
+++ src/component/MyCarousel/MyCarousel.js
@@ -1,48 +1,32 @@
 import React from 'react';
 
 import Carousel from 'react-bootstrap/Carousel';
+import one from '../../assets/images/immersion.jpg'
+import two from '../../assets/images/indigenous.jpg'
+import three from '../../assets/images/laiwa.jpg'
+import  { carousels }  from '../../data/carousels'
+import './style.css'
 
-const MyCarousel = (props) => {
+const MyCarousel = () => {
+  const data = carousels.carousels
+  console.log(data[0])
+  
   return (
     <Carousel controls={false}>
       <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="https://images.unsplash.com/photo-1528642474498-1af0c17fd8c3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80"
-          alt="First slide"
-        />
-        <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
+        <img className="d-block" src={one} alt="First slide" />
+        <Carousel.Caption><h3>{data[0].title}</h3></Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="https://images.unsplash.com/photo-1517732306149-e8f829eb588a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=752&q=80"
-          alt="Second slide"
-        />
-
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
+        <img className="d-block" src={two} alt="Second slide" />
+        <Carousel.Caption><h3>{data[1].title}</h3></Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="https://images.unsplash.com/photo-1517732306149-e8f829eb588a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=752&q=80"
-          alt="Third slide"
-        />
-
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-        </Carousel.Caption>
+        <img className="d-block" src={three} alt="Third slide" />
+        <Carousel.Caption><h3>{data[2].title}</h3></Carousel.Caption>
       </Carousel.Item>
     </Carousel>
-
-  );
+  )
 }
 
 export default MyCarousel;
