97f5069f6bde6c625844747ade22e51c909a52d4
matt011 <38515217+matt011@users.noreply.github.com>
2020-02-22 17:09:25 +0800

    make location cards home

--- src/component/pages/HomePage/HomePage.js
+++ src/component/pages/HomePage/HomePage.js
@@ -1,10 +1,32 @@
 import React from 'react';
 import MyCarousel from '../../MyCarousel';
+import { locations } from '../../../data/locations'
 
 const HomePage = (props) => {
+  const data = locations.locations;
+  console.log(data);
+
+  // const renderLocationCards = () => {
+
+  //   return (
+
+  //   );
+  
+
+  // }
+
   return (
     <>
     <MyCarousel />
+    <div> Location Cards </div>
+
+    <ol>
+    {data.map(loc => (
+        <li key={loc.id}>{loc.location}</li>
+      ))}
+    </ol>
+
+  
     </> 
 
   );
---
+++ src/data/locations.js
@@ -0,0 +1,110 @@
+export const locations = {
+  locations: [
+    {
+      id: 1,
+      location: "Cabangan Zambales",
+      community: "Aeta Community",
+      locOverview:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Risus in hendrerit gravida rutrum quisque non. Non nisi est sit amet facilisis magna etiam tempor orci. Gravida in fermentum et sollicitudin ac orci phasellus. Praesent tristique magna sit amet purus. Sagittis purus sit amet volutpat consequat mauris nunc congue nisi.",
+      comOverview:
+        "Amet consectetur adipiscing elit ut aliquam purus sit amet luctus. Et pharetra pharetra massa massa ultricies mi quis hendrerit dolor. Egestas quis ipsum suspendisse ultrices gravida dictum fusce. Mi tempus imperdiet nulla malesuada pellentesque. Egestas quis ipsum suspendisse ultrices gravida dictum fusce ut.\n Tortor at risus viverra adipiscing at in tellus. Nisl purus in mollis nunc. Vulputate dignissim suspendisse in est ante in nibh mauris cursus. Amet cursus sit amet dictum sit amet justo donec. Enim facilisis gravida neque convallis. \n Amet consectetur adipiscing elit ut aliquam purus sit amet luctus. Et pharetra pharetra massa massa ultricies mi quis hendrerit dolor. Egestas quis ipsum suspendisse ultrices gravida dictum fusce. Mi tempus imperdiet nulla malesuada pellentesque. Egestas quis ipsum suspendisse ultrices gravida dictum fusce ut.",
+      expect:
+        "Amet consectetur adipiscing elit ut aliquam purus sit amet luctus. Et pharetra pharetra massa massa ultricies mi quis hendrerit dolor. Egestas quis ipsum suspendisse ultrices gravida dictum fusce. Mi tempus imperdiet nulla malesuada pellentesque. Egestas quis ipsum suspendisse ultrices gravida dictum fusce ut. Tortor at risus viverra adipiscing at in tellus. Nisl purus in mollis nunc. Vulputate dignissim suspendisse in est ante in nibh mauris cursus. Amet cursus sit amet dictum sit amet justo donec. Enim facilisis gravida neque convallis.",
+      prepare: [
+        "Set of clothes for 3 days",
+        "Toiletries/hygienic materials",
+        "Boots",
+        "Flash light",
+        "Pocket money"
+      ]
+    },
+    {
+      id: 2,
+      location: "Batanes",
+      community: "Ivatan Community",
+      locOverview:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Risus in hendrerit gravida rutrum quisque non. Non nisi est sit amet facilisis magna etiam tempor orci. Gravida in fermentum et sollicitudin ac orci phasellus. Praesent tristique magna sit amet purus. Sagittis purus sit amet volutpat consequat mauris nunc congue nisi.",
+      comOverview:
+        "Amet consectetur adipiscing elit ut aliquam purus sit amet luctus. Et pharetra pharetra massa massa ultricies mi quis hendrerit dolor. Egestas quis ipsum suspendisse ultrices gravida dictum fusce. Mi tempus imperdiet nulla malesuada pellentesque. Egestas quis ipsum suspendisse ultrices gravida dictum fusce ut. \nTortor at risus viverra adipiscing at in tellus. Nisl purus in mollis nunc. Vulputate dignissim suspendisse in est ante in nibh mauris cursus. Amet cursus sit amet dictum sit amet justo donec. Enim facilisis gravida neque convallis. Amet consectetur adipiscing elit ut aliquam purus sit amet luctus. Et pharetra pharetra massa massa ultricies mi quis hendrerit dolor. Egestas quis ipsum suspendisse ultrices gravida dictum fusce. Mi tempus imperdiet nulla malesuada pellentesque. Egestas quis ipsum suspendisse ultrices gravida dictum fusce ut.",
+      expect:
+        "Amet consectetur adipiscing elit ut aliquam purus sit amet luctus. Et pharetra pharetra massa massa ultricies mi quis hendrerit dolor. Egestas quis ipsum suspendisse ultrices gravida dictum fusce. Mi tempus imperdiet nulla malesuada pellentesque. Egestas quis ipsum suspendisse ultrices gravida dictum fusce ut. Tortor at risus viverra adipiscing at in tellus. Nisl purus in mollis nunc. Vulputate dignissim suspendisse in est ante in nibh mauris cursus. Amet cursus sit amet dictum sit amet justo donec. Enim facilisis gravida neque convallis.",
+      prepare: [
+        "Set of clothes for 3 days",
+        "Toiletries/hygienic materials",
+        "Boots",
+        "Flash light",
+        "Pocket money"
+      ]
+    },
+    {
+      id: 3,
+      location: "Tanay Rizal",
+      community: "Dumagat tribe",
+      locOverview:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Risus in hendrerit gravida rutrum quisque non. Non nisi est sit amet facilisis magna etiam tempor orci. Gravida in fermentum et sollicitudin ac orci phasellus. Praesent tristique magna sit amet purus. Sagittis purus sit amet volutpat consequat mauris nunc congue nisi.",
+      comOverview:
+        "Amet consectetur adipiscing elit ut aliquam purus sit amet luctus. Et pharetra pharetra massa massa ultricies mi quis hendrerit dolor. Egestas quis ipsum suspendisse ultrices gravida dictum fusce. Mi tempus imperdiet nulla malesuada pellentesque. Egestas quis ipsum suspendisse ultrices gravida dictum fusce ut. Tortor at risus viverra adipiscing at in tellus. Nisl purus in mollis nunc. Vulputate dignissim suspendisse in est ante in nibh mauris cursus. Amet cursus sit amet dictum sit amet justo donec. Enim facilisis gravida neque convallis. Amet consectetur adipiscing elit ut aliquam purus sit amet luctus. Et pharetra pharetra massa massa ultricies mi quis hendrerit dolor. Egestas quis ipsum suspendisse ultrices gravida dictum fusce. Mi tempus imperdiet nulla malesuada pellentesque. Egestas quis ipsum suspendisse ultrices gravida dictum fusce ut.",
+      expect:
+        "Amet consectetur adipiscing elit ut aliquam purus sit amet luctus. Et pharetra pharetra massa massa ultricies mi quis hendrerit dolor. Egestas quis ipsum suspendisse ultrices gravida dictum fusce. Mi tempus imperdiet nulla malesuada pellentesque. Egestas quis ipsum suspendisse ultrices gravida dictum fusce ut. Tortor at risus viverra adipiscing at in tellus. Nisl purus in mollis nunc. Vulputate dignissim suspendisse in est ante in nibh mauris cursus. Amet cursus sit amet dictum sit amet justo donec. Enim facilisis gravida neque convallis.",
+      prepare: [
+        "Set of clothes for 3 days",
+        "Toiletries/hygienic materials",
+        "Boots",
+        "Flash light",
+        "Pocket money"
+      ]
+    }
+  ]
+};
+// export const locations = {
+//    locations: [
+//    [
+//       "id" : 1,
+//       "location": "Cabangan Zambales",
+//       "community": "Aeta Community",
+//       "locOverview": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Risus in hendrerit gravida rutrum quisque non. Non nisi est sit amet facilisis magna etiam tempor orci. Gravida in fermentum et sollicitudin ac orci phasellus. Praesent tristique magna sit amet purus. Sagittis purus sit amet volutpat consequat mauris nunc congue nisi.",
+//       "comOverview": "Amet consectetur adipiscing elit ut aliquam purus sit amet luctus. Et pharetra pharetra massa massa ultricies mi quis hendrerit dolor. Egestas quis ipsum suspendisse ultrices gravida dictum fusce. Mi tempus imperdiet nulla malesuada pellentesque. Egestas quis ipsum suspendisse ultrices gravida dictum fusce ut.\n Tortor at risus viverra adipiscing at in tellus. Nisl purus in mollis nunc. Vulputate dignissim suspendisse in est ante in nibh mauris cursus. Amet cursus sit amet dictum sit amet justo donec. Enim facilisis gravida neque convallis. \n Amet consectetur adipiscing elit ut aliquam purus sit amet luctus. Et pharetra pharetra massa massa ultricies mi quis hendrerit dolor. Egestas quis ipsum suspendisse ultrices gravida dictum fusce. Mi tempus imperdiet nulla malesuada pellentesque. Egestas quis ipsum suspendisse ultrices gravida dictum fusce ut.",
+//       "expect": "Amet consectetur adipiscing elit ut aliquam purus sit amet luctus. Et pharetra pharetra massa massa ultricies mi quis hendrerit dolor. Egestas quis ipsum suspendisse ultrices gravida dictum fusce. Mi tempus imperdiet nulla malesuada pellentesque. Egestas quis ipsum suspendisse ultrices gravida dictum fusce ut. Tortor at risus viverra adipiscing at in tellus. Nisl purus in mollis nunc. Vulputate dignissim suspendisse in est ante in nibh mauris cursus. Amet cursus sit amet dictum sit amet justo donec. Enim facilisis gravida neque convallis.",
+//       "prepare":
+//       [
+//          "Set of clothes for 3 days",
+//          "Toiletries/hygienic materials",
+//          "Boots",
+//          "Flash light",
+//          "Pocket money"
+//       ]
+//    ]
+//    {
+//       "id" : 2,
+//       "location": "Batanes",
+//       "community": "Ivatan Community",
+//       "locOverview": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Risus in hendrerit gravida rutrum quisque non. Non nisi est sit amet facilisis magna etiam tempor orci. Gravida in fermentum et sollicitudin ac orci phasellus. Praesent tristique magna sit amet purus. Sagittis purus sit amet volutpat consequat mauris nunc congue nisi.",
+//       "comOverview": "Amet consectetur adipiscing elit ut aliquam purus sit amet luctus. Et pharetra pharetra massa massa ultricies mi quis hendrerit dolor. Egestas quis ipsum suspendisse ultrices gravida dictum fusce. Mi tempus imperdiet nulla malesuada pellentesque. Egestas quis ipsum suspendisse ultrices gravida dictum fusce ut. \nTortor at risus viverra adipiscing at in tellus. Nisl purus in mollis nunc. Vulputate dignissim suspendisse in est ante in nibh mauris cursus. Amet cursus sit amet dictum sit amet justo donec. Enim facilisis gravida neque convallis. Amet consectetur adipiscing elit ut aliquam purus sit amet luctus. Et pharetra pharetra massa massa ultricies mi quis hendrerit dolor. Egestas quis ipsum suspendisse ultrices gravida dictum fusce. Mi tempus imperdiet nulla malesuada pellentesque. Egestas quis ipsum suspendisse ultrices gravida dictum fusce ut.",
+//       "expect": "Amet consectetur adipiscing elit ut aliquam purus sit amet luctus. Et pharetra pharetra massa massa ultricies mi quis hendrerit dolor. Egestas quis ipsum suspendisse ultrices gravida dictum fusce. Mi tempus imperdiet nulla malesuada pellentesque. Egestas quis ipsum suspendisse ultrices gravida dictum fusce ut. Tortor at risus viverra adipiscing at in tellus. Nisl purus in mollis nunc. Vulputate dignissim suspendisse in est ante in nibh mauris cursus. Amet cursus sit amet dictum sit amet justo donec. Enim facilisis gravida neque convallis.",
+//       "prepare":
+//       [
+//          "Set of clothes for 3 days",
+//          "Toiletries/hygienic materials",
+//          "Boots",
+//          "Flash light",
+//          "Pocket money"
+//       ]
+//    },
+//    {
+//       "id" : 3,
+//       "location": "Cabangan Zambales",
+//       "community": "Dumagat tribe",
+//       "locOverview": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Risus in hendrerit gravida rutrum quisque non. Non nisi est sit amet facilisis magna etiam tempor orci. Gravida in fermentum et sollicitudin ac orci phasellus. Praesent tristique magna sit amet purus. Sagittis purus sit amet volutpat consequat mauris nunc congue nisi.",
+//       "comOverview": "Amet consectetur adipiscing elit ut aliquam purus sit amet luctus. Et pharetra pharetra massa massa ultricies mi quis hendrerit dolor. Egestas quis ipsum suspendisse ultrices gravida dictum fusce. Mi tempus imperdiet nulla malesuada pellentesque. Egestas quis ipsum suspendisse ultrices gravida dictum fusce ut. Tortor at risus viverra adipiscing at in tellus. Nisl purus in mollis nunc. Vulputate dignissim suspendisse in est ante in nibh mauris cursus. Amet cursus sit amet dictum sit amet justo donec. Enim facilisis gravida neque convallis. Amet consectetur adipiscing elit ut aliquam purus sit amet luctus. Et pharetra pharetra massa massa ultricies mi quis hendrerit dolor. Egestas quis ipsum suspendisse ultrices gravida dictum fusce. Mi tempus imperdiet nulla malesuada pellentesque. Egestas quis ipsum suspendisse ultrices gravida dictum fusce ut.",
+//       "expect": "Amet consectetur adipiscing elit ut aliquam purus sit amet luctus. Et pharetra pharetra massa massa ultricies mi quis hendrerit dolor. Egestas quis ipsum suspendisse ultrices gravida dictum fusce. Mi tempus imperdiet nulla malesuada pellentesque. Egestas quis ipsum suspendisse ultrices gravida dictum fusce ut. Tortor at risus viverra adipiscing at in tellus. Nisl purus in mollis nunc. Vulputate dignissim suspendisse in est ante in nibh mauris cursus. Amet cursus sit amet dictum sit amet justo donec. Enim facilisis gravida neque convallis.",
+//       "prepare":
+//       [
+//          "Set of clothes for 3 days",
+//          "Toiletries/hygienic materials",
+//          "Boots",
+//          "Flash light",
+//          "Pocket money"
+//       ]
+//    }
+// ]
+// }
