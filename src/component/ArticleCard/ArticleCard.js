27b4d09efc7d1f3d288d2c40ee3abdd02e38f97c
Krizza Bullecer <krizzabullecer@gmail.com>
2020-02-22 23:30:25 +0800

    add images

--- src/component/ArticleCard/ArticleCard.js
+++ src/component/ArticleCard/ArticleCard.js
@@ -1,12 +1,13 @@
 import React from 'react'
 
 import Card from 'react-bootstrap/Card'
-import aeta from '../../assets/images/aeta.jpg'
+import aeta from '../../assets/images/aeta2.jpg'
+import fish from '../../assets/images/fish2.jpg'
 import './style.css'
 
 const ArticleCard = props => (
     <Card className="ArticleCard">
-        <Card.Img variant="top" src={aeta} />
+        <Card.Img variant="top" src={0 === props.id ? aeta : fish} style={{maxHeight: '350px'}}/>
         <Card.Body>
             <Card.Title>{props.title}</Card.Title>
             <small>{props.date}</small>
