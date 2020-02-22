66436839ddad2cdd2c3cda8e64427b18c7e5a249
Krizza Bullecer <krizzabullecer@gmail.com>
2020-02-22 15:46:41 +0800

    added LocationCard component

---
+++ src/component/LocationCard/LocationCard.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './style.css'
+
+const LocationCard = props => {
+    return(
+        <div className="LocationCard">
+            Hello
+        </div>
+    )
+}
+
+export default LocationCard
