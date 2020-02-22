651c1049a25f62be3488f40e8c0264561fdb490c
Krizza Bullecer <krizzabullecer@gmail.com>
2020-02-22 15:46:03 +0800

    added LocationPage component

---
+++ src/component/pages/LocationPage/LocationPage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import LocationCard from '../../LocationCard'
+
+import './style.css'
+
+const LocationPage = () => (
+    <div className="LocationPage">
+        <LocationCard />
+    </div>
+)
+
+export default LocationPage
