a0aec1ec0cbcb786b87fbc73d40db7d9b95b4e43
Krizza Bullecer <krizzabullecer@gmail.com>
2020-02-22 15:50:58 +0800

    added dynamic change background image

--- src/component/LocationCard/LocationCard.js
+++ src/component/LocationCard/LocationCard.js
@@ -3,9 +3,11 @@ import React from 'react'
 import './style.css'
 
 const LocationCard = props => {
+    document.getElementsByClassName('LocationCard').style.backgroundImage = "url('img_tree.png')";
+
     return(
         <div className="LocationCard">
-            Hello
+            
         </div>
     )
 }
