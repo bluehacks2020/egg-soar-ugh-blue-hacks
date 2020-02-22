ec1cd8bb5269790353a7edec8a078c910d8668be
Krizza Bullecer <krizzabullecer@gmail.com>
2020-02-22 17:24:17 +0800

    fetch props from other component

--- src/component/LocationCard/LocationCard.js
+++ src/component/LocationCard/LocationCard.js
@@ -3,11 +3,12 @@ import React from 'react'
 import zambales from '../../assets/images/zambales.jpg'
 import './style.css'
 
-const LocationCard = props => {
+const LocationCard = ({ id, location}) => {
     return(
-        <div className="LocationCard" style={{backgroundImage: `url(${zambales})`, backgroundSize: 'cover'}}>
-            <div>Zambales</div>
-            <div>Aeta Community</div>
+        <div className="LocationCard"
+            style={{backgroundImage: `url(${zambales})`, backgroundSize: 'cover'}}
+            location-id={id}>
+            <h6>{location}</h6>
         </div>
     )
 }
