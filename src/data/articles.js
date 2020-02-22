d0a0ef18cae3148a5ed39371ea28a7ef38fb13b9
Krizza Bullecer <krizzabullecer@gmail.com>
2020-02-22 23:30:20 +0800

    add unique id

--- src/data/articles.js
+++ src/data/articles.js
@@ -1,11 +1,13 @@
 export const articles = {
     articles: [
         {
+            "id": 1,
             "title": "How Living with Fisherfolk Community has Changed my Perspective",
             "date": "Dec 23, 2019",
             "preview": "Ever since I started working in a food company, I’ve always wondered what it feels like to be the ones to gather the raw food that we..."
         },
         {
+            "id": 0,
             "title": "I Celebrated my Valentines with the Most Lovable People Ever",
             "date": "Feb 17, 2020",
             "preview": "This year, I connected our Valentine’s Day celebration to an ongoing service learning project..."
