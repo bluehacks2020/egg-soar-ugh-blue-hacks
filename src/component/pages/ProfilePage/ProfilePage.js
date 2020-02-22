13614e437f1601520e88ec8c6082dbd7ab13f1c2
Kyle Ordona <janzrel.ordona@obf.ateneo.edu>
2020-02-22 17:48:21 +0800

    add profilePage

---
+++ src/component/pages/ProfilePage/ProfilePage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import profile from '../../../data/profile'
+import './style.css'
+
+const ProfilePage = () => (
+    <div className="ProfilePage">
+        <div className=""
+        {profile.name}
+    </div>
+)
+
+export default ProfilePage
