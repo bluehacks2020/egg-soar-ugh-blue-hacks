42effde27a085d2ddb4d2b7e6a0f93fd9988fd80
Krizza Bullecer <krizzabullecer@gmail.com>
2020-02-23 00:29:50 +0800

    update nav style

--- src/component/Navigation/Navigation.js
+++ src/component/Navigation/Navigation.js
@@ -2,7 +2,6 @@ import React from 'react';
 
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 import Form from 'react-bootstrap/Form';
 import FormControl from 'react-bootstrap/FormControl';
 import './style.css';
@@ -25,14 +24,9 @@ const Navigation = (props) => {
           <FormControl type="text" placeholder="Search" className="mr-sm-2" />
         </Form>
         <Nav.Link href="#home">Home</Nav.Link>
-        <Nav.Link href="#link">Link</Nav.Link>
-        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-          <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-          <NavDropdown.Divider />
-          <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-        </NavDropdown>
+        <Nav.Link href="#link">Profile</Nav.Link>
+        <Nav.Link href="#link">Settings</Nav.Link>
+        <Nav.Link href="#link">Logout</Nav.Link>
       </Nav>
     </Navbar.Collapse>
   </Navbar>
