c5ca9117d329c3db8c37fa5e673f038f9f617dca
Krizza Bullecer <krizzabullecer@gmail.com>
2020-02-23 00:02:16 +0800

    modified footer component

--- src/component/Footer/Footer.js
+++ src/component/Footer/Footer.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
+import './style.css'
 
-const Footer = (props) => {
-  return <div>Footer</div>
-}
+const Footer = (props) => (
+  <Fragment>
+    <div className="myFooter">
+      <p className="footer-brand"></p>
+      <hr/>
+      <p>Hello</p>
+    </div>
+    <div className="copyrightInfo">© 2020 Kultura, Inc. All rights reserved.</div>
+  </Fragment>
+)
 
 export default Footer;
