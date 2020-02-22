d1869277062dd1b01379a8d4b6114e184fd75f28
Krizza Bullecer <krizzabullecer@gmail.com>
2020-02-22 21:12:17 +0800

    add on scroll function

--- src/component/Navigation/Navigation.js
+++ src/component/Navigation/Navigation.js
@@ -8,6 +8,13 @@ import FormControl from 'react-bootstrap/FormControl';
 import './style.css';
 
 const Navigation = (props) => {
+  let prevScroll = 0
+  window.addEventListener('scroll', () => {
+    const navbar = document.querySelector('nav.navbar')      
+    navbar.classList[window.scrollY > prevScroll ? 'add' : 'remove']('hide')
+    prevScroll = window.scrollY
+  })
+
   return (
     <Navbar className="color-nav" expand="lg" fixed="top">
     <Navbar.Brand href="#home" style={{color: '#f4f4f4'}}></Navbar.Brand>
